ce5b45c5fe297038de66a663e85f71006236ed85
RuichangBao <1294838346@qq.com>
2020-05-14 16:52:17 +0800

    服务器序列化反序列化

--- TestProtobufServer/game/src/main/java/com/testServer/game/App.java
+++ TestProtobufServer/game/src/main/java/com/testServer/game/App.java
@@ -16,32 +16,10 @@ public class App
 	{
 		System.out.println("Hello World!");
 		Request request=new Request(1,2);
+		byte[]data= MySerializerUtil.serialize(request,Request.class);
+		Request request2=MySerializerUtil.deSerialize(data, Request.class);
 		
+		System.out.println(request2.toString());
 	}
 	
-//	/**
-//	 * 序列化为protostuff格式，体积非常小
-//	 * 
-//	 * @param obj
-//	 * @return
-//	 */
-//	public static <T> byte[] serializer_protobufIOUtil(T obj)
-//	{
-//		Class<T> clazz = (Class<T>) obj.getClass();
-//		LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
-//		try
-//		{
-//			Schema<T> schema = getSchema(clazz);
-//
-//		        return ProtobufIOUtil.toByteArray(obj, schema, buffer);//
-//		}
-//		catch (Exception e)
-//		{
-//			throw new IllegalStateException(e.getMessage(), e);
-//		}
-//		finally
-//		{
-//			buffer.clear();
-//		}
-//	}
 }
